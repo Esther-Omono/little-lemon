f43471b518c8fd54ce53566cdf7834b24fb18ec3
Esther-Omono <enitanatakere@gmail.com>
2026-01-08 22:52:41 +0100

    Added responsiveness to the header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,20 +1,47 @@
+import { useState } from 'react';
 import Logo from '../assets/logo.jpg';
 import '../styles/header.css';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header>
-      <img src={Logo} width={250} alt='Little Lemon Logo' />
-      <nav>
+      <img src={Logo} width={250} alt='Little Lemon Logo' className='logo' />
+
+      <button
+        className={`hamburger ${isMenuOpen ? 'open' : ''}`}
+        onClick={toggleMenu}
+        aria-label='Toggle menu'
+      >
+        <span className='hamburger-line'></span>
+        <span className='hamburger-line'></span>
+        <span className='hamburger-line'></span>
+      </button>
+
+      <nav className={isMenuOpen ? 'open' : ''}>
         <ul>
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>MENU</li>
-          <li>RESERVATIONS</li>
-          <li>ORDER ONLINE</li>
-          <li>LOGIN</li>
+          <li onClick={closeMenu}>HOME</li>
+          <li onClick={closeMenu}>ABOUT</li>
+          <li onClick={closeMenu}>MENU</li>
+          <li onClick={closeMenu}>RESERVATIONS</li>
+          <li onClick={closeMenu}>ORDER ONLINE</li>
+          <li onClick={closeMenu}>LOGIN</li>
         </ul>
       </nav>
+
+      <div
+        className={`overlay ${isMenuOpen ? 'open' : ''}`}
+        onClick={closeMenu}
+      ></div>
     </header>
   );
 };
