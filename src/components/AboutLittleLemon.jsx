97c4b260b318537b554516772ef4b03208edb88b
Esther-Omono <enitanatakere@gmail.com>
2026-01-11 22:48:50 +0100

    Added about and footer sections on the home page with responsiveness

--- src/components/AboutLittleLemon.jsx
+++ src/components/AboutLittleLemon.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import '../styles/aboutlittlelemon.css';
+import About1 from '../assets/about-image1.jpg';
+import About2 from '../assets/about-image2.jpg';
 
 const AboutLittleLemon = () => {
-  return <div>AboutLittleLemon</div>;
+  return (
+    <section className='about-section'>
+      <div className='about-container'>
+        <div className='about-text'>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat
+            purus sed dapibus cursus. Maecenas blandit sapien vel libero
+            ultricies, eget tempus neque vulputate. Sed facilisis convallis
+            dolor, non feugiat sem tincidunt ut. Aliquam ut magna id metus
+            congue pretium. Duis lacinia, orci sit amet porta tincidunt, eros
+            dui semper velit, nec imperdiet turpis nulla quis odio. Maecenas
+            imperdiet interdum facilisis. Etiam ut augue vehicula, molestie
+            turpis ut, gravida lorem. Proin tincidunt nibh id ipsum eleifend
+            vestibulum. Donec bibendum at est vel tempor. Aenean ut suscipit
+            nisi. Nulla facilisi. Suspendisse ultricies rutrum mi et laoreet.
+            Nunc eget nisi in est venenatis varius in sit amet libero. Aliquam
+            vulputate ex id ligula interdum, ut venenatis est suscipit.
+          </p>
+        </div>
+        <div className='about-images'>
+          <img src={About1} alt='Little Lemon Chef' />
+          <img src={About2} alt='Little Lemon Chef' />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AboutLittleLemon;
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import '../styles/footer.css';
+import footerImage from '../assets/footer-image.png';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaPhoneAlt,
+  FaTwitter,
+  FaYoutube,
+} from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+import { FaLocationDot } from 'react-icons/fa6';
 
 const Footer = () => {
-  return <footer>This is footer</footer>;
+  return (
+    <footer className='footer-section'>
+      <div className='footer-container'>
+        <img className='footer-logo' src={footerImage} alt='Little Lemon' />
+        <div className='footer-nav'>
+          <h4>Sitemap</h4>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+          </ul>
+        </div>
+        <div className='footer-contact'>
+          <h4>Contact us</h4>
+          <div>
+            <FaLocationDot size={20} />
+            <p>678 Pisa Ave, Chicago, IL 60611</p>
+          </div>
+
+          <div>
+            <FaPhoneAlt size={20} />
+            <p>(312) 593-2744</p>
+          </div>
+
+          <div>
+            <MdEmail size={20} />
+            <p>info@littlelemon.com</p>
+          </div>
+        </div>
+        <div className='footer-socials'>
+          <h4>Connect with us</h4>
+
+          <FaFacebook size={25} className='icon' />
+          <FaTwitter size={25} className='icon' />
+          <FaInstagram size={25} className='icon' />
+          <FaYoutube size={25} className='icon' />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
